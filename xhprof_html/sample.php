93bb6126e70271e510785233acc4c5b60f045342
Marcelo Vani <marcellovani@yahoo.co.uk>
2016-11-12 00:17:08 +0000

    Moved examples inside docroot

---
+++ xhprof_html/sample.php
@@ -0,0 +1,23 @@
+<?php
+
+function bar($x) {
+  if ($x > 0) {
+    bar($x - 1);
+  }
+}
+
+function foo() {
+  for ($idx = 0; $idx < 5; $idx++) {
+    bar($idx);
+    $x = strlen("abc");
+  }
+}
+
+// Start profiling.
+include '../includes/xhprof_start.php';
+
+// run program
+foo();
+
+// End profiling.
+include '../includes/xhprof_end.php';
