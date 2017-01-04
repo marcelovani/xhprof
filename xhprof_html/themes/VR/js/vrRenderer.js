556d3c0b4009e23d45863155a7b2a16daf9ccd47
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-01-04 09:21:35 +0000

    Use require

--- xhprof_html/themes/VR/js/vrRenderer.js
+++ xhprof_html/themes/VR/js/vrRenderer.js
@@ -1,31 +1,21 @@
 var renderer;
-var renderers = {};
 var effect;
-renderers['3d'] = ["../../node_modules/three/examples/js/renderers/CSS3DRenderer.js"];
-renderers['vr'] = ["../../node_modules/three/examples/js/renderers/CSS3DStereoRenderer.js"];
-
 var activeRenderer = null;
 
-function changeRenderer(renderer) {
-	activeRenderer = renderer;
+function changeRenderer(_renderer) {
+	activeRenderer = _renderer;
 	jQuery('.group-renderer').removeClass('active');
-	jQuery('#' + renderer).addClass('active');
+	jQuery('#' + _renderer).addClass('active');
 }
 
-function loadRenderer(type) {
-	delete THREE.CSS3DRenderer;
-	delete THREE.CSS3DStereoRenderer;
-	for ( var i = 0; i < renderers[type].length; i ++ ) {
-		loadJS(renderers[type][i], updateRenderer, document.body);
-	}
+function updateRenderer() {
+	render(activeRenderer);
 }
 
-function initRenderer( type ) {
+function render( type ) {
 	switch ( type ) {
 		case '3d':
-			if ( typeof(THREE.CSS3DRenderer) !== 'function' ) {
-				loadRenderer( type );
-			} else {
+			require(['CSS3DRenderer'], function () {
 				if (renderer instanceof THREE.CSS3DRenderer) {
 					renderer.render( scene, camera );
 				}
@@ -37,13 +27,11 @@ function initRenderer( type ) {
 					container.appendChild( renderer.domElement );
 					renderer.setSize( window.innerWidth, window.innerHeight );
 				}
-			}
+			});
 			break;
 
 		case 'vr':
-			if ( typeof(THREE.CSS3DStereoRenderer) !== 'function' ) {
-				loadRenderer( type );
-			} else {
+			require(['CSS3DStereoRenderer'], function () {
 				if (renderer instanceof THREE.CSS3DStereoRenderer) {
 					effect.render( scene, camera );
 				}
@@ -59,11 +47,7 @@ function initRenderer( type ) {
 					effect.setSize( window.innerWidth, window.innerHeight );
 					effect.setEyeSeparation(3);
 				}
-			}
+			});
 			break;
 	}
-}
-
-function updateRenderer() {
-	initRenderer(activeRenderer);
 }
